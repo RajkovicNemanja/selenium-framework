ba6e4e5e94eda829091832a47150433f88fc913b
Nemanja Rajkovic <NemanjaRajkovic93@gmail.com>
2020-10-27 19:21:27 +0100

    html report generation supported

---
+++ src/main/java/core/ExtentReporter.java
@@ -0,0 +1,21 @@
+package core;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReporter {
+    private static ExtentReports reports;
+
+    public static ExtentReports getReportsObject()
+    {
+        String path = System.getProperty("user.dir") + "\\reports\\index.html";
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter(path);
+        sparkReporter.config().setReportName("Regression tests report");
+        sparkReporter.config().setDocumentTitle("Test results");
+
+        reports = new ExtentReports();
+        reports.attachReporter(sparkReporter);
+        reports.setSystemInfo("QA Engineer","Nemanja Rajkovic");
+        return reports;
+    }
+}
